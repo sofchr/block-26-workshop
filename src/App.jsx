cc959d0b7eecd665361f25194c9e21dc3127312e
Sofie Christensen <sofiec510@gmail.com>
2023-08-07 14:57:50 -0400

    dummy contact info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import ContactList from "./components/ContactList.jsx";
+import ContactRow from "./components/ContactRow.jsx";
 
 export default function App() {
   return (
